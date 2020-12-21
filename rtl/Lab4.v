e4954b9cf346288756b579a17da063cfaec90784
Che100r <spirrus1999@mail.ru>
2020-12-21 18:02:43 +0300

    Lab4 done

---
+++ rtl/Lab4.v
@@ -0,0 +1,113 @@
+module Lab4 (
+	input clk,
+	input reset,
+	input button
+);
+
+localparam yellow = 3'b000;
+localparam yellow_blinking = 3'b001;
+localparam green = 3'b010;
+localparam green_blinking = 3'b011;
+localparam red = 3'b100;
+localparam red_and_yellow = 3'b101;
+localparam crosswalk = 3'b110;
+
+reg [2:0] state;
+reg [2:0] next_state;
+reg work;
+
+always @(posedge clk) begin
+	if (reset) begin
+		state <= yellow_blinking;
+		work <= 0;
+	end
+	else begin
+		state = next_state;
+		work <= 1;
+	end
+end
+
+reg [2:0] button_syncroniser;
+
+always @(posedge clk)
+begin
+	button_syncroniser[0] <= button;
+	button_syncroniser[1] <= button_syncroniser[0];
+	button_syncroniser[2] <= button_syncroniser[1];
+end
+
+assign button_pressed = ~button_syncroniser[2] & button_syncroniser[1];
+reg [3:0] counter;
+reg crosswalk_button;
+
+always @(*) begin
+	if (reset)
+		crosswalk_button = 0;
+	else if (button_pressed)
+		crosswalk_button = 1;
+	else if (state == crosswalk)
+		crosswalk_button = 0;
+end
+
+always @(posedge clk) begin
+	if (work == 0) begin
+		next_state <= yellow_blinking;
+		counter = 4'd0;
+	end else begin
+	case (state)
+		yellow_blinking: if (work)
+			next_state <= green;
+
+		green: if (counter == 4'd15) begin
+			next_state <= green_blinking;
+			counter = 0;
+		end else begin
+			counter = counter + 1;
+			next_state <= green;
+		end
+
+		green_blinking: if (counter == 4'd5) begin
+			next_state <= yellow;
+			counter = 0;
+		end else begin
+			counter = counter + 1;
+			next_state <= green_blinking;
+		end
+
+		yellow: if (counter == 4'd3) begin
+			next_state <= red;
+			counter = 0;
+		end else begin
+			counter = counter + 1;
+			next_state <= yellow;
+		end
+
+		red: if (crosswalk_button)
+			next_state <= crosswalk;
+		else if (counter == 4'd10) begin
+			next_state <= red_and_yellow;
+			counter = 0;
+		end else begin
+			counter = counter + 1;
+			next_state <= red;
+		end
+
+		red_and_yellow: if (counter == 4'd5) begin
+			next_state <= green;
+			counter = 0;
+		end else begin
+			counter = counter + 1;
+			next_state <= red_and_yellow;
+		end
+
+		crosswalk: if (counter == 4'd10) begin
+			next_state <= red_and_yellow;
+			counter = 0;
+		end else begin
+			counter = counter + 1;
+			next_state <= crosswalk;
+		end
+	endcase
+	end
+end
+endmodule
---
+++ rtl/Lab4_tb.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+module Lab4_tb();
+
+localparam CLK_FREQ_MHZ   = 100;
+localparam CLK_SEMIPERIOD = ( 1000 / CLK_FREQ_MHZ ) / 2;
+
+reg clk;
+reg reset;
+reg button;
+
+Lab4 DUT
+(
+	.clk (clk),
+	.reset (reset),
+	.button (button)
+);
+
+initial begin
+	clk = 1'b1;
+	forever begin
+		#CLK_SEMIPERIOD clk=~clk;
+	end
+end
+
+initial begin
+	forever begin
+		button = 1;
+		#( 100 * CLK_SEMIPERIOD );
+		button = 0;
+		#( 2 * CLK_SEMIPERIOD );
+		button = 1;
+	end
+end
+
+initial begin
+	reset = 1'b0;
+	#( 2 * CLK_SEMIPERIOD );
+	reset = 1'b1;
+	#( 4 * CLK_SEMIPERIOD );
+	reset = 1'b0;
+end
+
+endmodule
